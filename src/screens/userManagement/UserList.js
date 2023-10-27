71e949b9b1783b804b3fbefa5ee4fa62a31ae89d
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2023-10-28 00:47:46 +0530

    add a search function to user MG

--- src/screens/userManagement/UserList.js
+++ src/screens/userManagement/UserList.js
@@ -10,6 +10,18 @@ export default function UserList() {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [searchkey, setsearchkey]             = useState('')
+    const [duplicateUser, setDuplicateUser] = useState([])
+
+    //search function
+    function filterBySearch() {        
+
+        const tempUser = duplicateUser.filter((user) =>
+            user.firstName.toLowerCase().includes(searchkey.toLowerCase())
+        )
+
+        setUsers(tempUser)
+    }
 
     // useEffect to fetch users data when the component mounts
     useEffect(() => {
@@ -21,6 +33,7 @@ export default function UserList() {
                 const response = await axios.get("/api/users/getallusers");
 
                 // Set the users data in the state
+                setDuplicateUser(response.data)
                 setUsers(response.data);
 
                 setLoading(false);
@@ -39,8 +52,8 @@ export default function UserList() {
         const doc = new jsPDF();
 
         // Define the columns and rows for the PDF table
-        const columns = ["UserID", "First Name", "Last Name", "Phone Number", "Email", "Is Admin"];
-        const rows = users.map(user => [user._id, user.firstName, user.lastName, user.phoneNumber, user.email, user.isAdmin ? "YES" : "NO"]);
+        const columns = ["UserID", "First Name", "Last Name", "Phone Number", "Email"];
+        const rows = users.map(user => [user._id, user.firstName, user.lastName, user.phoneNumber, user.email]);
 
         // Add the table to the PDF document
         doc.autoTable({
@@ -56,57 +69,76 @@ export default function UserList() {
     // Render the users data in a table
     return (
         <div className="row">
-            <div className="col-md-12">
+        <div className="col-md-12">
             <h1
-            style={{
-                fontSize: "30px",
-                color: "#C18239",
-                fontWeight: "bold",
+                style={{
+                    fontSize: "30px",
+                    color: "#C18239",
+                    fontWeight: "bold",
+                }}
+            >
+                Users
+            </h1>
+            <br />
+            {loading && <Loader />}
+
+            <div className="row">
+
+            <div className="col-md-6">
+                    <button
+                        onClick={generatePdfReport}
+                        className="btn btnColour"
+                    >
+                        <UilFileGraph />
+                        Generate PDF Report
+                    </button>
+                </div>
+
+                <div className="col-md-6">
+                    <div className="input-group mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Search by First Name"
+                            value={searchkey}
+                            onChange={(e) => setsearchkey(e.target.value)}
+                            onKeyUp={filterBySearch}
+                        />                        
+                    </div>
+                </div>
+
                 
-            }}
-            >Users</h1>
-                <br />
-                {loading && <Loader />}
-
-                <button
-                    onClick={generatePdfReport}
-                    className="btn btnColour mb-3 ml-3"
-                >
-                    <UilFileGraph />
-                    Generate PDF Report
-                </button>
-
-                <table
-                    class="table table-bordered"
-                    style={{ border: "solid 2px #C2BDB8" }}
-                >
-                    <thead style={{ backgroundColor: "#FCF9EF" }}>
-                        <tr>
-                            <th scope="col">UserID</th>
-                            <th scope="col">First Name</th>
-                            <th scope="col">Last Name</th>
-                            <th scope="col">Phone Number</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Is Admin</th>
-                        </tr>
-                    </thead>
-                    <tbody style={{ backgroundColor: "#E9E3D3" }}>
-                        {users.length &&
-                            users.map((user) => {
-                                return (
-                                    <tr>
-                                        <td>{user._id}</td>
-                                        <td>{user.firstName}</td>
-                                        <td>{user.lastName}</td>
-                                        <td>{user.phoneNumber}</td>
-                                        <td>{user.email}</td>
-                                        <td>{user.isAdmin ? "YES" : "NO"}</td>
-                                    </tr>
-                                );
-                            })}
-                    </tbody>
-                </table>
             </div>
+
+            <table
+                className="table table-bordered"
+                style={{ border: "solid 2px #C2BDB8" }}
+            >
+                <thead style={{ backgroundColor: "#FCF9EF" }}>
+                    <tr>
+                        <th scope="col">UserID</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Phone Number</th>
+                        <th scope="col">Email</th>
+                    </tr>
+                </thead>
+                <tbody style={{ backgroundColor: "#E9E3D3" }}>
+                    {users.length &&
+                        users.map((user) => {
+                            return (
+                                <tr>
+                                    <td>{user._id}</td>
+                                    <td>{user.firstName}</td>
+                                    <td>{user.lastName}</td>
+                                    <td>{user.phoneNumber}</td>
+                                    <td>{user.email}</td>
+                                </tr>
+                            );
+                        })}
+                </tbody>
+            </table>
         </div>
+    </div>
     )
 }
